3410b327abab6214dfdb1034ead3859c70c7ec81
Sébastien Eveno <sebastien.louis.eveno@gmail.com>
2024-08-15 08:25:46 +0200

    Add Seat entity

---
+++ TheatreBookingSystem.Tests/SeatTests.cs
@@ -0,0 +1,47 @@
+﻿using TheatreBookingSystem.Domain;
+
+namespace TheatreBookingSystem.Tests;
+
+public class SeatTests
+{
+	[Test]
+	public void Constructor_ShouldInitializeWithGivenIdAndNotBooked()
+	{
+		// Arrange
+		var seatId = Guid.NewGuid();
+
+		// Act
+		var seat = new Seat(seatId);
+
+		// Assert
+		Assert.That(seat.Id, Is.EqualTo(seatId));
+		Assert.That(seat.IsBooked, Is.False);
+	}
+
+	[Test]
+	public void Book_ShouldSetIsBookedToTrue_WhenSeatIsNotAlreadyBooked()
+	{
+		// Arrange
+		var seatId = Guid.NewGuid();
+		var seat = new Seat(seatId);
+
+		// Act
+		seat.Book();
+
+		// Assert
+		Assert.That(seat.IsBooked, Is.True);
+	}
+
+	[Test]
+	public void Book_ShouldThrowException_WhenSeatIsAlreadyBooked()
+	{
+		// Arrange
+		var seatId = Guid.NewGuid();
+		var seat = new Seat(seatId);
+		seat.Book(); // Book the seat first
+
+		// Act & Assert
+		var exception = Assert.Throws<InvalidOperationException>(() => seat.Book());
+		Assert.That(exception.Message, Is.EqualTo("Seat already booked"));
+	}
+}
